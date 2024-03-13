6f0b88c2e6cbd0faa6563aaea9227cf978675fb3
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-03-12 19:51:58 -0700

    Number by score fixed

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Script that lists the number of records eith the same score in the table
-SELECT score, COUNT (score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
