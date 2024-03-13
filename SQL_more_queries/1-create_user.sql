30067e58f8039f3106c0df82e7718deaec1bcdb6
AnitRodriguez <anitruthrodriguez@gmail.com>
2024-03-13 11:55:07 -0700

    fix syntax task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Script that lists all privileges of MySQL user
-CREATE USER IF NOT EXISTS 'user_0d_1' @ 'localhost' INDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' @ 'localhost' 
+INDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1' @ 'localhost';
 FLUSH PRIVILEGES;
